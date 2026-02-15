759cba16b924eb7fce07a9b848965dc108d9d11c
محمد عادل عبدالستار السملاوي <123871294+smellawy@users.noreply.github.com>
2026-02-15 19:27:08 +0200

    Refactor helper functions and improve formatting
    Refactor helpers functions for consistency and clarity.

--- helpers/index.js
+++ helpers/index.js
@@ -1,97 +1,68 @@
-(function (){
+(function () {
   'use strict';
+  const axios = require("axios");
+  const helpers = {};
 
-  var axios = require("axios");
-  var helpers = {};
+  // Error-handling middleware → MUST have 4 parameters
+  helpers.errorHandler = function (err, req, res, next) {
+    console.error(err.stack); // good for logs
 
-  /* Public: errorHandler is a middleware that handles your errors
-   *
-   * Example:
-   *
-   * var app = express();
-   * app.use(helpers.errorHandler);
-   * */
-
-  helpers.errorHandler = function(err, req, res, next) {
-    var ret = {
-      message: err.message,
-      error:   err
+    const status = err.status || 500;
+    const response = {
+      message: status === 500 ? 'Internal Server Error' : err.message,
+      // error: err   // ← uncomment only in development
     };
-    res.
-      status(err.status || 500).
-      send(ret);
+
+    res.status(status).json(response);
   };
 
-  helpers.sessionMiddleware = function(err, req, res, next) {
-    if(!req.cookies.logged_in) {
-      res.session.customerId = null;
+  // Normal middleware → 3 parameters
+  helpers.sessionMiddleware = function (req, res, next) {
+    if (!req.cookies.logged_in) {
+      req.session.customerId = null;
+    }
+    next(); // Don't forget this!
+  };
+
+  helpers.respondSuccessBody = function (res, body) {
+    helpers.respondStatusBody(res, 200, body);
+  };
+
+  helpers.respondStatusBody = function (res, statusCode, body) {
+    res.writeHead(statusCode);
+    res.write(body);
+    res.end();
+  };
+
+  helpers.respondStatus = function (res, statusCode) {
+    res.writeHead(statusCode);
+    res.end();
+  };
+
+  helpers.rewriteSlash = function (req, res, next) {
+    if (req.url.substr(-1) === '/' && req.url.length > 1) {
+      res.redirect(301, req.url.slice(0, -1));
+    } else {
+      next();
     }
   };
 
-  /* Responds with the given body and status 200 OK  */
-  helpers.respondSuccessBody = function(res, body) {
-    helpers.respondStatusBody(res, 200, body);
-  }
-
-  /* Public: responds with the given body and status
-   *
-   * res        - response object to use as output
-   * statusCode - the HTTP status code to set to the response
-   * body       - (string) the body to yield to the response
-   */
-  helpers.respondStatusBody = function(res, statusCode, body) {
-    res.writeHeader(statusCode);
-    res.write(body);
-    res.end();
-  }
-
-  /* Responds with the given statusCode */
-  helpers.respondStatus = function(res, statusCode) {
-    res.writeHeader(statusCode);
-    res.end();
-  }
-
-  /* Rewrites and redirects any url that doesn't end with a slash. */
-  helpers.rewriteSlash = function(req, res, next) {
-   if(req.url.substr(-1) == '/' && req.url.length > 1)
-       res.redirect(301, req.url.slice(0, -1));
-   else
-       next();
-  }
-
-  /* Public: performs an HTTP GET request to the given URL
-   *
-   * url  - the URL where the external service can be reached out
-   * res  - the response object where the external service's output will be yield
-   * next - callback to be invoked in case of error. If there actually is an error
-   *        this function will be called, passing the error object as an argument
-   *
-   * Examples:
-   *
-   * app.get("/users", function(req, res) {
-   *   helpers.simpleHttpRequest("http://api.example.org/users", res, function(err) {
-   *     res.send({ error: err });
-   *     res.end();
-   *   });
-   * });
-   */
-  helpers.simpleHttpRequest = function(url, res, next) {
-    axios.get(url)
-      .then(function(response) {
+  helpers.simpleHttpRequest = function (url, res, next) {
+    axios
+      .get(url)
+      .then(function (response) {
         helpers.respondSuccessBody(res, response.data);
       })
-      .catch(function(error) {
-        return next(error);
+      .catch(function (error) {
+        next(error); // pass to error handler
       });
-  }
+  };
 
-  /* TODO: Add documentation */
-  helpers.getCustomerId = function(req, env) {
-    // Check if logged in. Get customer Id
-    var logged_in = req.cookies.logged_in;
+  helpers.getCustomerId = function (req, env) {
+    const logged_in = req.cookies.logged_in;
 
     // TODO REMOVE THIS, SECURITY RISK
-    if (env == "development" && req.query.custId != null) {
+    if (env === "development" && req.query.custId != null) {
       return req.query.custId;
     }
 
@@ -99,11 +70,11 @@
       if (!req.session.id) {
         throw new Error("User not logged in.");
       }
-      // Use Session ID instead
       return req.session.id;
     }
 
     return req.session.customerId;
-  }
+  };
+
   module.exports = helpers;
-}());
+})();
